e542894f43f65559f49a0021437d8252b91c75fe
VarshaSriv <87266637+VarshaSriv@users.noreply.github.com>
2024-04-12 12:21:47 +0530

    removed  secret

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -15,5 +15,5 @@ provider "azurerm" {
   subscription_id = var.az_subscriptionid
   tenant_id = var.az_tenantid
   client_id = "ecc5de6d-e40a-48f0-8e30-15774826d28c"
-  client_secret = "Mrl8Q~YTsnTYnKbOaRKjoTPqME-YftvuNqiCYcMx"
+  client_secret = ""
 }
